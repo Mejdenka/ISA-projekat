a3e4ab35a497dea8555c37ee676429a4297dec0c
Jelena <jeca_997@hotmail.com>
2019-12-11 18:25:33 +0100

    Modal prozor za kliniku

---
+++ src/main/java/JV20/isapsw/controller/KlinikaController.java
@@ -0,0 +1,26 @@
+package JV20.isapsw.controller;
+
+
+import JV20.isapsw.model.Klinika;
+import JV20.isapsw.service.KlinikaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/klinike")
+public class KlinikaController {
+    @Autowired
+    private KlinikaService klinikaService;
+
+    @RequestMapping("/getAll")
+    @PreAuthorize("hasRole('USER')")
+    public List<Klinika> getAll() {
+        System.out.println("Uslo u klinike");
+        return this.klinikaService.findAll();
+    }
+}

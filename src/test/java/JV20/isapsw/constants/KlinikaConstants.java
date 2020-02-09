dcde66ee0206b97c4464fa38f9e44cd468caf86e
Marina Simic <simic.marina@outlook.com>
2020-02-09 23:43:45 +0100

    Final touch!!!!!!!!

--- src/test/java/JV20/isapsw/constants/KlinikaConstants.java
+++ src/test/java/JV20/isapsw/constants/KlinikaConstants.java
@@ -19,7 +19,6 @@ public class KlinikaConstants {
 
     public static final String IME_KLINIKE = "Klinika 1";
     public static final String OPIS_KLINIKE = "Prva klinika u centru.";
-    public static final Long ID_KLINIKE = 1L;
     public static final int PREGLEDI_SIZE = 1;
     public static final int LEKARI_SIZE = 2;
 
